5a8f6c73a81cb571b033fac49639ecb1842a97f9
Michael Diender <michael.diender@shinesolutions.com>
2016-12-02 10:52:38 +1100

    pass the component into the serverspec to run specific tests for the component

--- provisioners/serverspec.sh
+++ provisioners/serverspec.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+component=$1
 sudo gem install bundler --no-ri --no-rdoc
 cd /tmp/tests
 bundle install --path=vendor
-bundle exec rake spec
+bundle exec rake spec SPEC=spec/${component}_spec.rb
