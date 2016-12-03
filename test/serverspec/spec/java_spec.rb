ddc753738636a60c01ff9d2a79c58bcd0b802e79
Michael Diender <michael.diender@shinesolutions.com>
2016-12-03 21:24:39 +1100

    remove jce true (default false) don't think the machines need jce. use variables to check java version from hieradata

--- test/serverspec/spec/java_spec.rb
+++ test/serverspec/spec/java_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
-describe command('echo $JAVA_HOME /') do
-  its(:stdout) { should match '/opt/jdk1.8.0_112' }
+install_dir = @properties['jdk_oracle::install_dir']
+version = @properties['jdk_oracle::version']
+version_update = @properties['jdk_oracle::version_update']
+
+java_version = "1.#{version}.0_#{version_update}"
+java_home = "#{install_dir}/jdk#{java_version}"
+
+
+describe command('echo $JAVA_HOME') do
+  its(:stdout) { should match "#{java_home}" }
 end
 
-describe command('java -version /') do
-  its(:stderr) { should match /java version "1.8.0_112"/ }
+describe command('java -version') do
+  its(:stderr) { should match "java version \"#{java_version}\"" }
   its(:exit_status) { should eq 0 }
 end
