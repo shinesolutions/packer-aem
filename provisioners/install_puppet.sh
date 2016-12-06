a4c55f11af5fa0df53b827757081513549fded84
Michael Diender <michael.diender@shinesolutions.com>
2016-12-07 09:00:15 +1100

    need to use the pc1 collection url to get the latest version of puppet https://docs.puppet.com/puppet/4.7/puppet_collections.html

--- provisioners/install_puppet.sh
+++ provisioners/install_puppet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 sudo yum install puppet -y
 sudo puppet resource package puppet ensure=latest
 #sudo yum update -y
