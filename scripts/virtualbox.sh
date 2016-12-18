71e5d406c5e8664096e0f9b4b7ef13d8bb6b00f1
Cliffano Subagio <cliffano@gmail.com>
2016-12-18 16:35:13 +1100

    Clean up shellcheck errors.

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -5,12 +5,12 @@ VBOX_ISO=/home/vagrant/VBoxGuestAdditions.iso
 VBOX_MNTDIR=$(mktemp --tmpdir=/tmp -q -d -t vbox_mnt_XXXXXX)
 
 # Install tools
-mount -o loop $VBOX_ISO $VBOX_MNTDIR
-yes|sh $VBOX_MNTDIR/VBoxLinuxAdditions.run
+mount -o loop "$VBOX_ISO" "$VBOX_MNTDIR"
+yes|sh "$VBOX_MNTDIR/VBoxLinuxAdditions.run"
 
 # Clean up
-umount $VBOX_MNTDIR
-rm -rf $VBOX_MNTDIR
-rm -f $VBOX_ISO
+umount "$VBOX_MNTDIR"
+rm -rf "$VBOX_MNTDIR"
+rm -f "$VBOX_ISO"
 
 #TODO: use https://docs.puppet.com/puppet/latest/types/mount.html and https://docs.puppet.com/puppet/4.8/types/exec.html
