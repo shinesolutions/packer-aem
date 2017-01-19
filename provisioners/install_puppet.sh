73de549ac714080f26026ddf81f3bc928882d96d
Michael Diender <michael.diender@shinesolutions.com>
2017-01-19 11:55:33 +1100

    inherit environment variables -E

--- provisioners/install_puppet.sh
+++ provisioners/install_puppet.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 sudo yum check-update
 
-sudo wget http://google.com
+sudo -E wget http://google.com
 
-sudo wget https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+sudo -E wget https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 
 ls
 
-sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+sudo -E rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 
 
-sudo yum install puppet-agent -y
+sudo -E yum install puppet-agent -y
 
-sudo /opt/puppetlabs/bin/puppet resource package puppet ensure=latest
-sudo yum update -y
+sudo -E /opt/puppetlabs/bin/puppet resource package puppet ensure=latest
+sudo -E yum update -y
