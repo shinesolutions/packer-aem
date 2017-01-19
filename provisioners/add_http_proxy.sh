1bc57bb9b4c85314639359569615e51613bf5950
Michael Diender <michael.diender@shinesolutions.com>
2017-01-19 11:24:46 +1100

    disable proxy setting for now.

--- provisioners/add_http_proxy.sh
+++ provisioners/add_http_proxy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo "export http_proxy=$HTTP_PROXY" | sudo tee --append /etc/profile.d/http_proxy.sh
-echo "export https_proxy=$HTTPS_PROXY" | sudo tee --append /etc/profile.d/http_proxy.sh
-
-sudo chmod 755 /etc/profile.d/http_proxy.sh
-
-echo "export proxy=$HTTP_PROXY" | sudo tee --append /etc/yum.comf
+#echo "export http_proxy=$HTTP_PROXY" | sudo tee --append /etc/profile.d/http_proxy.sh
+#echo "export https_proxy=$HTTPS_PROXY" | sudo tee --append /etc/profile.d/http_proxy.sh
+#
+#sudo chmod 755 /etc/profile.d/http_proxy.sh
+#
+#echo "export proxy=$HTTP_PROXY" | sudo tee --append /etc/yum.comf
--- provisioners/install_puppet.sh
+++ provisioners/install_puppet.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-echo "http_proxy=$http_proxy"
-ping forwardproxy
-
 sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 sudo yum install puppet-agent -y
 sudo /opt/puppetlabs/bin/puppet resource package puppet ensure=latest
