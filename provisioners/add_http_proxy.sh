89f3484947eaaabef094b37ac3cbadc802a59e53
Michael Diender <michael.diender@shinesolutions.com>
2017-01-19 11:00:23 +1100

    add proxy to yum conf

--- provisioners/add_http_proxy.sh
+++ provisioners/add_http_proxy.sh
@@ -4,3 +4,5 @@ echo "export http_proxy=$HTTP_PROXY" | sudo tee --append /etc/profile.d/http_pro
 echo "export https_proxy=$HTTPS_PROXY" | sudo tee --append /etc/profile.d/http_proxy.sh
 
 sudo chmod 755 /etc/profile.d/http_proxy.sh
+
+echo "export proxy=$HTTP_PROXY" | sudo tee --append /etc/yum.comf
