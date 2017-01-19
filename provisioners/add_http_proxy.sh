8da0b3a4e6c5f64a7035b5ebe1e8f7b7fd17c0de
Michael Diender <michael.diender@shinesolutions.com>
2017-01-19 13:03:16 +1100

    enable setting proxy

--- provisioners/add_http_proxy.sh
+++ provisioners/add_http_proxy.sh
@@ -4,4 +4,4 @@
 echo "export http_proxy=$HTTP_PROXY" | sudo tee --append /etc/profile.d/proxy.sh
 echo "export https_proxy=$HTTPS_PROXY" | sudo tee --append /etc/profile.d/proxy.sh
 
-sudo chmod 755 /etc/profile.d/http_proxy.sh
+sudo chmod 755 /etc/profile.d/proxy.sh
