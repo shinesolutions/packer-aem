0770d19d12a707068819d95d197c075fc22dfa1f
Michael Diender <michael.diender@shinesolutions.com>
2017-01-19 15:41:58 +1100

    pass environment variables into sudo command

--- provisioners/puppet/modules/serverspec/files/serverspec.sh
+++ provisioners/puppet/modules/serverspec/files/serverspec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 component=$1
 staging_directory=$2
-sudo gem install bundler --no-ri --no-rdoc
+sudo -E gem install bundler --no-ri --no-rdoc
 cd /tmp/serverspec
 bundle install --path=vendor
 FACTER_packer_build_name=${component} FACTER_packer_staging_directory=${staging_directory} bundle exec rake spec SPEC=spec/${component}_spec.rb
