774e24e2a61db734c0e7cbe42dc75416e0a39cc6
Michael Diender <michael.diender@shinesolutions.com>
2017-01-19 12:47:03 +1100

    correct the version number to ensure

--- provisioners/install_puppet.sh
+++ provisioners/install_puppet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+#TODO: installation of puppet should be an option.
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install puppet-agent -y
-/opt/puppetlabs/bin/puppet resource package puppet ensure=4.8
+/opt/puppetlabs/bin/puppet resource package puppet ensure=1.8.2-1.el7
 yum update -y
