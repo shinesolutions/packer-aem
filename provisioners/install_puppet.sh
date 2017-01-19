a59c6aec7df4a31ceb43d082a5983cd63d296f0d
Michael Diender <michael.diender@shinesolutions.com>
2017-01-19 12:25:21 +1100

    ensure version 4.8 of puppet. specify puppet bin dir

--- provisioners/install_puppet.sh
+++ provisioners/install_puppet.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-which puppet
-puppet --version
-
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install puppet-agent -y
-/opt/puppetlabs/bin/puppet resource package puppet ensure=latest
+/opt/puppetlabs/bin/puppet resource package puppet ensure=4.8
 yum update -y
-
-which puppet
-puppet --version
