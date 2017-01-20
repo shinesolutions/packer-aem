79bd0dae88608e5e6615407af05abf8bcdc29a4f
Michael Diender <michael.diender@shinesolutions.com>
2017-01-20 13:41:40 +1100

    revert using puppet bin dir variable

--- provisioners/install_puppet.sh
+++ provisioners/install_puppet.sh
@@ -2,5 +2,5 @@
 #TODO: installation of puppet should be an option.
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install puppet-agent -y
-"$PUPPET_BIN_DIR/puppet resource package puppet ensure=1.8.2-1.el7"
+/opt/puppetlabs/bin/puppet resource package puppet ensure=1.8.2-1.el7
 #yum update -y
