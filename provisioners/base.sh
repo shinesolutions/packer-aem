cff2206c0fb7209bf4e14f0fbd5f479c3d69885f
Michael Diender <michael.diender@shinesolutions.com>
2017-01-27 11:46:09 +1100

    remove yum update -y - only perform what you need to for bootstrap

--- provisioners/base.sh
+++ provisioners/base.sh
@@ -2,4 +2,3 @@
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install puppet-agent -y
 "$PUPPET_BIN_DIR"/puppet resource package puppet ensure=latest
-yum update -y
