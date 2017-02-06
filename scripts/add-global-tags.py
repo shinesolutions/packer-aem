417eebb31df9d97604bd96df88610d513dabfd3f
Michael Diender <michael.diender@shinesolutions.com>
2017-02-06 16:24:23 +1100

    rename file to use '.yaml'. format the py file.

--- scripts/add-global-tags.py
+++ scripts/add-global-tags.py
@@ -26,25 +26,22 @@ def read_global_tags(file_name):
     tags = ruamel.yaml.load(file, Loader=ruamel.yaml.SafeLoader)
     file.close()
 
-    return [(entry['Key'],entry['Value']) for entry in global_tags['Tags']]
+    return [(entry['Key'], entry['Value']) for entry in global_tags['Tags']]
 
 
 def add_tags(global_tags, template, tag_key):
     template['builders'][0][tag_key].update(global_tags)
 
 
-template_dir=sys.argv[1]
+template_dir = sys.argv[1]
 tags_file_name = sys.argv[2]
-tag_keys=['run_tags','run_volume_tags','snapshot_tags','tags']
+tag_keys = ['run_tags', 'run_volume_tags', 'snapshot_tags', 'tags']
 
-global_tags=read_global_tags(tags_file_name)
-template_files=glob.glob(template_dir + "*.json")
+global_tags = read_global_tags(tags_file_name)
+template_files = glob.glob(template_dir + "*.json")
 for template_file in template_files:
-    template=read_template(template_file)
+    template = read_template(template_file)
     for tag_key in tag_keys:
         if tag_key in template['builders'][0]:
-            add_tags(global_tags,template, tag_key)
-    write_template(template_file,template)
-
-
-
+            add_tags(global_tags, template, tag_key)
+    write_template(template_file, template)
