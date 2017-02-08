f082551a398e4953b0b9705fc9ff2b22ee355893
Cliffano Subagio <cliffano@gmail.com>
2017-02-09 09:36:51 +1100

    Stop httpd after serverspec run, to clean up its pid. #19

--- provisioners/puppet/manifests/httpd.pp
+++ provisioners/puppet/manifests/httpd.pp
@@ -1,6 +1,9 @@
 stage { 'test':
   require => Stage['main']
 }
+stage { 'shutdown':
+  require => Stage['test'],
+}
 
 class { 'apache': }
 
@@ -9,4 +12,17 @@ class { 'serverspec':
   component => 'httpd',
   tries     => 15,
   try_sleep => 3,
+}
+
+class { 'httpd_shutdown':
+  stage => 'shutdown',
+}
+
+class httpd_shutdown {
+
+  exec { 'service httpd stop':
+    cwd  => '/tmp',
+    path => ['/usr/bin', '/usr/sbin'],
+  }
+
 }
