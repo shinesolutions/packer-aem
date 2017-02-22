edfae521d7bf71c4ed44090cf23fa0b62095ace5
Cliffano Subagio <cliffano@gmail.com>
2017-02-23 10:47:46 +1100

    Bump up ruby_aem version to 1.0.8

--- test/serverspec/spec/aem_base_spec.rb
+++ test/serverspec/spec/aem_base_spec.rb
@@ -5,5 +5,5 @@ describe package('nokogiri') do
 end
 
 describe package('ruby_aem') do
-  it { should be_installed.by('gem').with_version('1.0.7') }
+  it { should be_installed.by('gem').with_version('1.0.8') }
 end
