dc9653e33ba57f7371937f30ea44efe5abe340d5
James Sinclair <james.sinclair@shinesolutions.com>
2017-02-24 16:11:46 +1100

    Add `base.sh` script for RHEL 6 flavoured distros.

---
+++ provisioners/aws/rhel6/base.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o nounset
+set -o errexit
+
+rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+yum install puppet-agent -y
+"$PUPPET_BIN_DIR"/puppet resource package puppet-agent ensure=latest
+
+# Enable the rhui-REGION-rhel-server-optional to install ruby-devel
+yum-config-manager --enable rhui-REGION-rhel-server-optional
