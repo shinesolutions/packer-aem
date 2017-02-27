6792aff205989cd38479d91e86319d4c647400b6
James Sinclair <james.sinclair@shinesolutions.com>
2017-02-27 14:19:26 +1100

    Turn on Puppet debugging in base template.

--- provisioners/aws/rhel6/base.sh
+++ provisioners/aws/rhel6/base.sh
@@ -8,3 +8,8 @@ yum install puppet-agent -y
 
 # Enable the rhui-REGION-rhel-server-optional to install ruby-devel
 yum-config-manager --enable rhui-REGION-rhel-server-optional
+
+sleep 30
+while pgrep -f yum; do
+    sleep 5
+done
