545131d72274a8eb96ab0ddb50b338f83dfadae1
Andy Wang <andy.wang@shinesolutions.com>
2017-03-07 14:14:14 +1100

    adding check on running instance and launch configuration useage before deregistering

--- scripts/purge_old_amis.sh
+++ scripts/purge_old_amis.sh
@@ -1,12 +1,9 @@
-#!/bin/bash -x
-
-
-sudo yum -y install jq
+#!/bin/bash 
 
 AMI_ROLE_ARR=("Base AMI" "Java AMI" "AEM Author AMI" "AEM Publish AMI" "Apache HTTP Server AMI" "AEM Dispatcher AMI")
 
 for (( i=0; i < ${#AMI_ROLE_ARR[@]}; i++)); do
-  echo "${AMI_ROLE_ARR[$i]}"
+  echo "processing images for application role \"${AMI_ROLE_ARR[$i]}\" .... "
 
   OLD_AMIS=$(aws ec2 describe-images --owner self \
          --filters "Name=tag:Application Id,Values=Adobe Experience Manager (AEM)"\
@@ -14,8 +11,21 @@ for (( i=0; i < ${#AMI_ROLE_ARR[@]}; i++)); do
          --region ap-southeast-2 | jq -r '."Images"|sort_by(."CreationDate")|.[0:-3]|.[]."ImageId"')
 
   for OLD_AMI in $OLD_AMIS; do
+    LCG_COUNT=$(aws autoscaling describe-launch-configurations \
+               --query 'LaunchConfigurations[?ImageId==`'$OLD_AMI'`].LaunchConfigurationName'| jq 'length')
+    if [ $LCG_COUNT -gt 0 ]; then
+      echo "Not deregistering image $OLD_AMI because it is still used by some launch configurations"
+      continue
+    fi
+    RUNNING_COUNT=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" \
+                   --query 'Reservations[].Instances[?ImageId==`'$OLD_AMI'`].InstanceId' | jq 'flatten|length')
+    if [ $RUNNING_COUNT -gt 0 ]; then
+      echo "Not deregistering image $OLD_AMI becuase it is still used by some runningn instances"
+      continue
+    fi
+    echo -n "Deregistering staled image $OLD_AMI ...."
     aws ec2 deregister-image --image-id "$OLD_AMI" --region ap-southeast-2
     sleep 1
+    echo "Done"
   done
 done
-
