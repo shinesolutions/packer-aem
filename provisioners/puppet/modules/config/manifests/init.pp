fc5a68f498d4d2a08d9c8bde47c0976921a60a86
James Sinclair <james.sinclair@shinesolutions.com>
2017-03-09 12:11:24 +1100

    Do an initial Puppet run so we can use the `puppet-gem` package provider successfully.

--- provisioners/puppet/modules/config/manifests/init.pp
+++ provisioners/puppet/modules/config/manifests/init.pp
@@ -16,8 +16,16 @@ class config () {
   # needed for running Serverspec, used for testing baked AMIs and provisioned EC instances
   package { [ 'gcc', 'ruby-devel', 'zlib-devel' ]:
     ensure  => 'installed',
-  }
+  } ->
   package { [ 'bundler', 'io-console' ]:
     provider => 'gem',
+  } ->
+  package { 'nokogiri':
+    ensure   => '1.6.8.1',
+    provider => 'puppet_gem',
+  } ->
+  package { 'ruby_aem':
+    ensure   => '1.0.8',
+    provider => 'puppet_gem',
   }
 }
