0e54a98dfda4ee7ac84a06e0efea35642cf134c8
Andy Wang <andy.wang@shinesolutions.com>
2017-03-14 13:46:15 +1100

    adding region to aws commands

--- scripts/purge_old_amis.sh
+++ scripts/purge_old_amis.sh
@@ -12,14 +12,16 @@ for (( i=0; i < ${#AMI_ROLE_ARR[@]}; i++)); do
 
   for OLD_AMI in $OLD_AMIS; do
     LCG_COUNT=$(aws autoscaling describe-launch-configurations \
-               --query 'LaunchConfigurations[?ImageId==`'"$OLD_AMI"'`].LaunchConfigurationName'| jq 'length')
+               --query 'LaunchConfigurations[?ImageId==`'"$OLD_AMI"'`].LaunchConfigurationName' \
+               --region ap-southeast-2 | jq 'length')
     if [ "$LCG_COUNT" -gt 0 ]; then
       echo "Not deregistering image $OLD_AMI because it is still used by some launch configurations"
       continue
     fi
     RUNNING_COUNT=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running"\
                       "Name=image-id,Values=$OLD_AMI" \
-                   --query 'Reservations[].Instances[].InstanceId' | jq 'flatten|length')
+                   --query 'Reservations[].Instances[].InstanceId' \
+                   --region ap-southeast-2 | jq 'flatten|length')
     if [ "$RUNNING_COUNT" -gt 0 ]; then
       echo "Not deregistering image $OLD_AMI becuase it is still used by some runningn instances"
       continue
