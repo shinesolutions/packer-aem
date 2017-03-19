956e14c885e2f208e2fc7fc2ba6b8c82761cba4c
Andy Wang <andy.wang@shinesolutions.com>
2017-03-20 10:20:08 +1100

    using default value to counter awscli different output behaviors on different platforms

--- scripts/purge_old_amis.sh
+++ scripts/purge_old_amis.sh
@@ -10,7 +10,7 @@ for (( i=0; i < ${#AMI_ROLE_ARR[@]}; i++)); do
                     "Name=tag:Application Role,Values=${AMI_ROLE_ARR[$i]}" \
          --region ap-southeast-2 | jq -r '."Images"|sort_by(."CreationDate")|.[0:-3]|.[]."ImageId"')
 
-  if [ "${OLD_AMIS}empty" = "empty" ]; then
+  if [ "${OLD_AMIS}EMPTY" = "EMPTY" ]; then
     echo "No stale images found for role \"${AMI_ROLE_ARR[$i]}\"."
     continue
   fi
@@ -19,14 +19,17 @@ for (( i=0; i < ${#AMI_ROLE_ARR[@]}; i++)); do
     LCG_COUNT=$(aws autoscaling describe-launch-configurations \
                --query 'LaunchConfigurations[?ImageId==`'"$OLD_AMI"'`].LaunchConfigurationName' \
                --region ap-southeast-2 | jq 'length')
+    LCG_COUNT="${LCG_COUNT:-0}"
     if [ "$LCG_COUNT" -gt 0 ]; then
       echo "Not deregistering image $OLD_AMI because it is still used by some launch configurations"
       continue
     fi
+
     RUNNING_COUNT=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running"\
                       "Name=image-id,Values=$OLD_AMI" \
                    --query 'Reservations[].Instances[].InstanceId' \
                    --region ap-southeast-2 | jq 'flatten|length')
+    RUNNING_COUNT="${RUNNING_COUNT:-0}"
     if [ "$RUNNING_COUNT" -gt 0 ]; then
       echo "Not deregistering image $OLD_AMI becuase it is still used by some runningn instances"
       continue
