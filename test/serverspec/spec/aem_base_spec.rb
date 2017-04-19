f2575a33f58c05ea004cc328790b7bced634c18b
Cliffano Subagio <cliffano@gmail.com>
2017-04-19 23:20:42 +1000

    Upgrade ruby_aem to 1.0.18 .

--- test/serverspec/spec/aem_base_spec.rb
+++ test/serverspec/spec/aem_base_spec.rb
@@ -5,5 +5,5 @@ describe package('nokogiri') do
 end
 
 describe package('ruby_aem') do
-  it { should be_installed.by('gem').with_version('1.0.16') }
+  it { should be_installed.by('gem').with_version('1.0.18') }
 end
