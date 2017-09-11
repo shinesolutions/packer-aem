7f4f2c3bd686af5479592e024d7ec26f698cb65d
Stephen Shim <stephen.shim@shinesolutions.com>
2017-09-11 12:01:59 +1000

    Add RHEL 7 O/S AMI, basic configuration.

--- provisioners/bash/rhel7-puppet-agent.sh
+++ provisioners/bash/rhel7-puppet-agent.sh
@@ -8,4 +8,5 @@ rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install puppet-agent epel-release
 
 # Enable the rhui-REGION-rhel-server-optional to install ruby-devel
+rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum-config-manager --enable rhui-REGION-rhel-server-optional
