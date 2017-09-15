dfc124c96cde95898e9f6588f568d434685de500
Stephen Shim <stephen.shim@shinesolutions.com>
2017-09-15 16:38:38 +1000

    Add Unified AEM AMI (author/publisher/dispatcher) - 3 in one. Serverspec file uniaem_spec.rb is a duplicate of dispatcher_spec.rb

---
+++ provisioners/puppet/manifests/uniaem.pp
@@ -0,0 +1,3 @@
+include ::config::dispatcher
+#include ::config::publish
+#include ::config::author
---
+++ test/serverspec/spec/uniaem_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+apache_package = @hiera.lookup('apache::apache_name', nil, @scope)
+apache_package ||= 'httpd'
+
+describe package("#{apache_package}") do
+  it { should be_installed }
+end
+
+describe service('httpd') do
+  it { should be_enabled }
+#  it { should be_running }
+end
+
+# TODO: check disapatcher installation.
+# dispatcher module installation differs depending on os and apache version.
+if os[:family] == 'redhat'
+
+
+
+
+
+end
