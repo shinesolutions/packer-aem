24595e9c2a917bd67598ce2307258724c44786cf
Stephen Shim <stephen.shim@shinesolutions.com>
2017-10-04 10:44:17 +1100

    Re-enable hotfix installation that was disabled during testing.

--- provisioners/puppet/modules/config/manifests/aem_cfp3.pp
+++ provisioners/puppet/modules/config/manifests/aem_cfp3.pp
@@ -4,27 +4,27 @@ class config::aem_cfp3() {
     group   => 'adobe/cq620/hotfix',
     version => '1.2',
   }
-  # -> config::aem_install_package { 'cq-6.2.0-hotfix-12785':
-  #   group                       => 'adobe/cq620/hotfix',
-  #   version                     => '7.0',
-  #   restart                     => true,
-  #   post_install_sleep_secs     => 150,
-  #   post_login_page_ready_sleep => 30,
-  # }
-  # -> config::aem_install_package { 'aem-service-pkg':
-  #   file_name => 'AEM-6.2-Service-Pack-1-6.2.SP1.zip',
-  #   group     => 'adobe/cq620/servicepack',
-  #   version   => '6.2.SP1',
-  # }
-  # -> config::aem_install_package { 'cq-6.2.0-sp1-cfp':
-  #   file_name               => 'AEM-6.2-SP1-CFP3-3.0.zip',
-  #   group                   => 'adobe/cq620/cumulativefixpack',
-  #   post_install_sleep_secs => 900,
-  #   version                 => '3.0',
-  # }
-  # -> config::aem_install_package { 'cq-6.2.0-hotfix-15607':
-  #   group   => 'adobe/cq620/hotfix',
-  #   version => '1.0',
-  # }
+  -> config::aem_install_package { 'cq-6.2.0-hotfix-12785':
+    group                       => 'adobe/cq620/hotfix',
+    version                     => '7.0',
+    restart                     => true,
+    post_install_sleep_secs     => 150,
+    post_login_page_ready_sleep => 30,
+  }
+  -> config::aem_install_package { 'aem-service-pkg':
+    file_name => 'AEM-6.2-Service-Pack-1-6.2.SP1.zip',
+    group     => 'adobe/cq620/servicepack',
+    version   => '6.2.SP1',
+  }
+  -> config::aem_install_package { 'cq-6.2.0-sp1-cfp':
+    file_name               => 'AEM-6.2-SP1-CFP3-3.0.zip',
+    group                   => 'adobe/cq620/cumulativefixpack',
+    post_install_sleep_secs => 900,
+    version                 => '3.0',
+  }
+  -> config::aem_install_package { 'cq-6.2.0-hotfix-15607':
+    group   => 'adobe/cq620/hotfix',
+    version => '1.0',
+  }
 
 }
