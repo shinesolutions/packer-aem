d44c018d5ac93e23f95cef634070e5839f20069f
Stephen Shim <stephen.shim@shinesolutions.com>
2017-11-08 15:30:06 +1100

    Rename variables and ensure files copied are named local.yaml and 99_local.json respectively.

--- scripts/setconfig.sh
+++ scripts/setconfig.sh
@@ -3,13 +3,13 @@ set -o nounset
 set -o errexit
 
 if [ "$#" -le 1 ] || [ "$#" -gt 2 ]; then
-  echo 'Usage: ./setconfig.sh <local_hiera_path> <local_packer_path>'
+  echo 'Usage: ./setconfig.sh <local_hiera_file> <local_packer_file>'
   exit 1
 fi
 
-local_hiera_path=$1
-local_packer_path=$2
+local_hiera_file=$1
+local_packer_file=$2
 
 echo "Copying custom files"
-cp -vf "$local_hiera_path" conf/hieradata/
-cp -vf "$local_packer_path" vars/
+cp -vf "$local_hiera_file" conf/hieradata/local.yaml
+cp -vf "$local_packer_file" vars/99_local.json
