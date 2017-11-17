26f97189126707024cfad88afcf60d197f4cc7a2
Stephen Shim <stephen.shim@shinesolutions.com>
2017-11-17 11:11:47 +1100

    Force installation as rpms may be installed in source AMIs

--- provisioners/bash/rhel7-puppet-agent.sh
+++ provisioners/bash/rhel7-puppet-agent.sh
@@ -4,9 +4,9 @@ set -o errexit
 
 yum -y upgrade
 
-rpm -ivh https://yum.puppetlabs.com/puppet5/puppet-release-el-7.noarch.rpm
+rpm -ivh --force https://yum.puppetlabs.com/puppet5/puppet-release-el-7.noarch.rpm
 yum -y install puppet-agent epel-release
 
 # Enable the rhui-REGION-rhel-server-optional to install ruby-devel
-rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh --force https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum-config-manager --enable rhui-REGION-rhel-server-optional
