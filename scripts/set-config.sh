75fc8aa30add00acbc18e306ee6455e9fc4efced
Cliffano Subagio <cliffano@gmail.com>
2017-11-27 22:53:05 +1100

    Ensure base directory programmatically rather than adding assumption note.

--- scripts/set-config.sh
+++ scripts/set-config.sh
@@ -3,9 +3,11 @@ set -o nounset
 set -o errexit
 
 if [ "$#" -le 1 ] || [ "$#" -gt 3 ]; then
-  echo 'Usage: ./setconfig.sh <local_hiera_file> <local_packer_file> <local_tag_file>'
+  echo 'Usage: ./set-config.sh <local_hiera_file> <local_packer_file> <local_tag_file>'
   exit 1
 fi
+
+BASEDIR=$(dirname "$0")
 
 local_hiera_file=$1
 local_packer_file=$2
@@ -16,5 +18,4 @@ cp -vf "$local_hiera_file" conf/hieradata/local.yaml
 cp -vf "$local_packer_file" vars/99_local.json
 
 echo "Executing global tag addition script: $local_tag_file"
-# Assume current working folder is packer-aem/
-python ./scripts/add-global-tags.py ./templates/ "$local_tag_file"
+python $BASEDIR/add-global-tags.py $BASEDIR/../templates/ "$local_tag_file"
