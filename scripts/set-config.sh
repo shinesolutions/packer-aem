b37a398c504a487e956c0abc55ba687b128e3fde
Cliffano Subagio <cliffano@gmail.com>
2017-11-27 23:09:20 +1100

    Reorder args and rename vars to be consistent with doco.

--- scripts/set-config.sh
+++ scripts/set-config.sh
@@ -3,19 +3,19 @@ set -o nounset
 set -o errexit
 
 if [ "$#" -le 1 ] || [ "$#" -gt 3 ]; then
-  echo 'Usage: ./set-config.sh <local_hiera_file> <local_packer_file> <local_tag_file>'
+  echo 'Usage: ./set-config.sh <tags_file> <hieradata_file> <packer_vars_file>'
   exit 1
 fi
 
 BASEDIR=$(dirname "$0")
 
-local_hiera_file=$1
-local_packer_file=$2
-local_tag_file=$3
+tags_file=$1
+hieradata_file=$2
+packer_vars_file=$3
 
-echo "Copying custom files"
-cp -vf "$local_hiera_file" conf/hieradata/local.yaml
-cp -vf "$local_packer_file" vars/99_local.json
+echo "Setting up Hieradata and Packer vars configuration files..."
+cp -vf "$hieradata_file" $BASEDIR/../conf/hieradata/local.yaml
+cp -vf "$packer_vars_file" $BASEDIR/../vars/99_local.json
 
-echo "Executing global tag addition script: $local_tag_file"
-python $BASEDIR/add-global-tags.py $BASEDIR/../templates/ "$local_tag_file"
+echo "Setting up AWS tags..."
+python $BASEDIR/add-global-tags.py $BASEDIR/../templates/ "$tags_file"
