825650156050398c34f2107f81a5c5aae6c1d166
Cliffano Subagio <cliffano@gmail.com>
2018-02-02 12:25:35 +1100

    Move Collectd configuration from packer-aem to aem-aws-stack-provisioner.

--- provisioners/puppet/manifests/author.pp
+++ provisioners/puppet/manifests/author.pp
@@ -2,10 +2,6 @@ include ::config::base
 
 include aem_curator::install_java
 
-if $::config::base::install_collectd {
-  include aem_curator::config_collectd
-}
-
 include aem_curator::install_author
 
 if $::config::base::install_cloudwatchlogs {
