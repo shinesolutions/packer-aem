474ca6a8f8e58f4dd9bf2f825d4052f3403c64b9
grumpyolddev <ramsesvejar@hotmail.com>
2018-03-25 15:47:20 +1100

    Issue/60/install puppet for amazon linux (#61)
    * installing puppet to amazon linux (not puppet5 as it depends on systemd and amazon linux does not have it)
    
    * downgrading inspec to be compatible with ruby version 2.1.9
    
    * adding root_volume_name as a variable due to the root device name is different on RHEL and Amazon Linux
    
    * Change root volume size to match latest master.
    
    * Revert root volume name for now due to not being required by other os types.
    
    * Reset config init to latest master, reverting back to ruby 2.2 is not an option.

--- provisioners/bash/amazon-linux.sh
+++ provisioners/bash/amazon-linux.sh
@@ -2,6 +2,13 @@
 set -o nounset
 set -o errexit
 
-# Placeholder for anything Amazon Linux specific shell provisioning
+yum -y upgrade
+rpm -ivh --force https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+yum -y install puppet-agent
+
+# when installing puppet5, we got this error as Amazon Linux does not have systemd
+# Error: Package: puppet-agent-5.3.3-1.el7.x86_64 (puppet5) Requires: systemd
+#sudo rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
+#yum -y install puppet-agent
 
 exit 0
