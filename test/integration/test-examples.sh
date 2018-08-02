a19cfe0cb84bf75873740275a6cd5715fd45f12d
Cliffano Subagio <cliffano@gmail.com>
2018-08-02 11:15:33 +1000

    Add non-local integration test. Fix parameters in integration test.

--- test/integration/test-examples.sh
+++ test/integration/test-examples.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# This script is used for integration testing AEM environments using local
-# repos, which allows the user to decide whether to use master or a feature
-# branch that's being worked on for each of the repos.
-# The repositories must be located at the same directory level.
-# The AEM environments are created using examples user configurations.
-
 if [[ "$#" -lt 1 ]] || [[ "$#" -gt 4 ]]; then
   echo "Usage: ${0} <test_id> [platform_type] [os_type] [aem_version]"
   exit 1
@@ -14,8 +8,8 @@ fi
 
 test_id=$1
 platform_type=${2:-aws}
-aem_version=${3:-aem64}
-os_type=${4:-rhel7}
+os_type=${3:-rhel7}
+aem_version=${4:-aem64}
 
 for component in author-publish-dispatcher author publish dispatcher java
 do
