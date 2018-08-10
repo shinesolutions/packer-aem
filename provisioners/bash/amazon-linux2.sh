da21166d4acf8e2a9a5a075f01e0d40738fab041
Peter Zurzolo <peter.zurzolO@shinesolutions.com>
2018-08-10 10:59:56 +1000

    updated amazon-linux2 bash script to include epel install/usage

--- provisioners/bash/amazon-linux2.sh
+++ provisioners/bash/amazon-linux2.sh
@@ -3,6 +3,11 @@ set -o nounset
 set -o errexit
 
 yum -y upgrade
+# sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
-yum -y install puppet-agent
+yum -y install puppet-agent epel-release
+
+# Enable the rhui-REGION-rhel-server-optional to install ruby-devel
+rpm -ivh --force https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum-config-manager --enable rhui-REGION-rhel-server-optional
