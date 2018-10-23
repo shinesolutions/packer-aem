01d9d434df32005cd8b774c6386d2010879d3aed
Eng Nuth <engnuth@eng.nuth@shinesolutions.com>
2018-10-23 14:21:07 +1100

    Fixes to support Amazon Linux 2

--- provisioners/bash/amazon-linux2.sh
+++ provisioners/bash/amazon-linux2.sh
@@ -3,10 +3,14 @@ set -o nounset
 set -o errexit
 
 yum -y upgrade
-# sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
+# install puppet5
 rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
-yum -y install puppet-agent epel-release
+yum -y install puppet-agent
+
+# install Inspec
+yum -y install gcc-c++
+/opt/puppetlabs/puppet/bin/gem install inspec
 
 # Enable the rhui-REGION-rhel-server-optional to install ruby-devel
 rpm -ivh --force https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
