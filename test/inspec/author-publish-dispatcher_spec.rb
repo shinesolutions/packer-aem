5a195ceabdc73645de5fbfcd3936ca9b51e62169
Michael Diender <michael.diender@shinesolutions.com>
2018-12-19 14:12:15 +1100

    check httpd service on amazon linux 2 using systemd

--- test/inspec/author-publish-dispatcher_spec.rb
+++ test/inspec/author-publish-dispatcher_spec.rb
@@ -146,9 +146,26 @@ describe package(apache_package) do
   it { should be_installed }
 end
 
-describe service('httpd') do
-  it { should be_enabled }
-  # it { should be_running }
+# inspec version 1.51.6. doesn't support Amazon Linux 2. It assumes it uses Upstart.
+# inspec version is locked to 1.51.6 to use train version 0.32 because it doesn't have an aws-sdk dependency:
+# https://github.com/inspec/inspec/blob/v1.51.6/inspec.gemspec#L29
+# inspec supports amazon linux 2 from version v2.1.30 (2018-04-05)
+# https://github.com/inspec/inspec/blob/master/CHANGELOG.md#v2130-2018-04-05
+# remove this bespoke handling once upgraded.
+if %w[amazon].include?(os[:name]) && !os[:release].start_with?('20\d\d')
+
+  describe systemd_service(apache_package) do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+else
+
+  describe service(apache_package) do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
 end
 
 describe port(apache_http_port) do
