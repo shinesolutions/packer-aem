eea79bea5c0d716bb98fa5296d2249ffaf5625d6
Cliffano Subagio <cliffano@gmail.com>
2019-01-09 09:28:10 +1100

    Add yum-utils, Development Tools, and sudo on Amazon Linux 2 provisioners, mostly to support Amazon Linux 2 Docker image base.

--- provisioners/bash/amazon-linux2.sh
+++ provisioners/bash/amazon-linux2.sh
@@ -2,6 +2,9 @@
 set -o nounset
 set -o errexit
 
+# yum-utils is needed by Amazon Linux 2 Docker image which doesn't include yum-config-manager
+yum -y install yum-utils
+
 yum -y upgrade
 
 # install puppet5
@@ -11,3 +14,9 @@ yum -y install puppet-agent
 # Enable the rhui-REGION-rhel-server-optional to install ruby-devel
 rpm -ivh --force https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum-config-manager --enable rhui-REGION-rhel-server-optional
+
+# Install development tools needed to natively build Nokogiri (a dependency of ruby_aem)
+yum -y groupinstall 'Development Tools'
+
+# AEM and Apache httpd provisioning will create users with certain sudo access
+yum install -y sudo
