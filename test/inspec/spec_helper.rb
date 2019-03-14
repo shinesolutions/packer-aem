ff75df4fb2bfe9c4f157e6b85dc1ac7687a3d930
Phillip Inman <phillip.inman@shinesolutions.com>
2019-03-14 14:02:36 +1100

    Really fixed the TODO now

--- test/inspec/spec_helper.rb
+++ test/inspec/spec_helper.rb
@@ -8,6 +8,6 @@ def init_conf
     '::packer_build_name' => Facter.value(:packer_build_name),
     '::os' => Facter.value(:os)
   }
-  #TODO: Fix this when inspec has a path from hiera 3 to 5
+  # TODO: Fix this when inspec has a path from hiera 3 to 5
   @hiera = Hiera.new(config: Facter.value(:packer_staging_dir) + '/hiera3.yaml')
 end
