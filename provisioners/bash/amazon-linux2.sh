08edd5eed8968e9663116278074877dbdce37199
Phillip Inman <phillip.inman@shinesolutions.com>
2019-03-29 13:56:29 +1100

    inital modification for conditional yum updates

--- provisioners/bash/amazon-linux2.sh
+++ provisioners/bash/amazon-linux2.sh
@@ -2,27 +2,39 @@
 set -o nounset
 set -o errexit
 
-# Tomcat 8.x was moved to amazon-linux-extras https://aws.amazon.com/amazon-linux-2/faqs/#Amazon_Linux_Extras
-# need to enable the package in order to access it via yum from Puppet manifest
-# enabling tomcat8.5 will set Tomcat 8.5 as the default tomcat package installation
-amazon-linux-extras enable tomcat8.5
+printenv
 
-# yum-utils is needed by Amazon Linux 2 Docker image which doesn't include yum-config-manager
-yum -y install yum-utils
+if [ -n "$UPDATE_OS" ]
+then
+    if [ "$UPDATE_OS" = "false" ]
+    then
+        echo "Skipping OS Updates..."
+        exit 0
+    fi
+else
+    echo "UPDATE_OS undefined, Bash Provisioner proceeding to run Updates..."
+    # Tomcat 8.x was moved to amazon-linux-extras https://aws.amazon.com/amazon-linux-2/faqs/#Amazon_Linux_Extras
+    # need to enable the package in order to access it via yum from Puppet manifest
+    # enabling tomcat8.5 will set Tomcat 8.5 as the default tomcat package installation
+    amazon-linux-extras enable tomcat8.5
 
-yum -y upgrade
+    # yum-utils is needed by Amazon Linux 2 Docker image which doesn't include yum-config-manager
+    yum -y install yum-utils
 
-# install puppet5
-rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
-yum -y install puppet-agent
+    yum -y upgrade
 
-# Enable the rhui-REGION-rhel-server-optional to install ruby-devel
-rpm -ivh --force https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum-config-manager --enable rhui-REGION-rhel-server-optional
+    # install puppet5
+    rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
+    yum -y install puppet-agent
 
-# Install development tools needed to natively build Nokogiri (a dependency of ruby_aem)
-# TODO: reduce the footprint and only install the ones required by Nokogiri
-yum -y groupinstall 'Development Tools'
+    # Enable the rhui-REGION-rhel-server-optional to install ruby-devel
+    rpm -ivh --force https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    yum-config-manager --enable rhui-REGION-rhel-server-optional
 
-# AEM and Apache httpd provisioning will create users with certain sudo access
-yum install -y sudo
+    # Install development tools needed to natively build Nokogiri (a dependency of ruby_aem)
+    # TODO: reduce the footprint and only install the ones required by Nokogiri
+    yum -y groupinstall 'Development Tools'
+
+    # AEM and Apache httpd provisioning will create users with certain sudo access
+    yum install -y sudo
+fi
