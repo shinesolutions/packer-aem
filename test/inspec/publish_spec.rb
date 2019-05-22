6d451910d3c58f66c42768f52574156dcf642d63
Cliffano Subagio <cliffano@gmail.com>
2019-05-22 15:04:17 +1000

    Disable install directory InSpec test due to refactoring of aem-healthcheck package lifecycle.

--- test/inspec/publish_spec.rb
+++ test/inspec/publish_spec.rb
@@ -77,11 +77,13 @@ describe file('/etc/puppetlabs/puppet/publish.yaml') do
   it { should be_grouped_into 'root' }
 end
 
-# Ensure that AEM install directory is empty at the end of provisioning
-# in order to avoid AEM from reinstalling the same package during environment
-# stand up. This scenario is a potential risk of unexpected repository size
-# growth, e.g. a bug in AEM 6.2 where a large AEM package would get reinstalled
-# and added to the repo, but not compacted for a period of time.
-describe Dir.empty?("#{aem_base}/aem/publish/crx-quickstart/install") do
-  it { should be true }
-end
+# TODO: due to the aem-healthcheck life cycle change in AEM OpenCloud 4.0.0, we need to modify the check
+#       below to ensure that install directory only contains one file and that file should be aem-healthcheck-content-version.zip
+# # Ensure that AEM install directory is empty at the end of provisioning
+# # in order to avoid AEM from reinstalling the same package during environment
+# # stand up. This scenario is a potential risk of unexpected repository size
+# # growth, e.g. a bug in AEM 6.2 where a large AEM package would get reinstalled
+# # and added to the repo, but not compacted for a period of time.
+# describe Dir.empty?("#{aem_base}/aem/publish/crx-quickstart/install") do
+#   it { should be true }
+# end
