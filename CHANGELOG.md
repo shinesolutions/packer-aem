e5cd59efa77693cf9f23fa455099d666b56adae7
Michael Bloch <michael.bloch@shinesolutions.com>
2019-08-02 12:36:12 +1000

    Updated process of config module to install packages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 - Upgrade aem_curator to 2.7.0
+- Update config hiera data files to contain a dict of packages to install on OS
+- Updated process of config module to install packages.
 
 ## 4.7.0 - 2019-07-24
 ### Changed
--- provisioners/puppet/modules/config/manifests/init.pp
+++ provisioners/puppet/modules/config/manifests/init.pp
@@ -14,6 +14,7 @@
 # Copyright © 2017	Shine Solutions Group, unless otherwise noted.
 #
 class config (
+  $package_manager_packages = {},
 ) {
   # Ensure we have a working FQDN <=> IP mapping.
   host { $facts['fqdn']:
@@ -21,30 +22,31 @@ class config (
     ip           => $facts['ipaddress'],
     host_aliases => $facts['hostname'],
   }
-  # needed for running Serverspec, used for testing baked AMIs and provisioned EC instances
-  package { [ 'gcc', 'gcc-c++', 'ruby-devel', 'zlib-devel' ]:
-    ensure  => 'installed',
-  # needed for ffi (a ruby_aem dependency) native compilation
-  } -> package { [ 'autoconf', 'automake', 'libtool' ]:
-    ensure => installed,
-  } -> package { 'io-console':
-    provider => 'gem',
-  # TODO: upgrade to bundler >= 2.0.1 when all machine images includes Ruby >= 2.3.0
-  # This bundler is used for the initial gem installation on the host
-  } -> package { 'bundler':
-    ensure   => '1.17.3',
-    provider => 'gem',
-  } -> package { 'nokogiri':
-    ensure   => '1.8.5',
-    provider => 'puppet_gem',
-  } -> package { 'ruby_aem':
-    ensure   => '3.2.1',
-    provider => 'puppet_gem',
-  } -> package { 'ruby_aem_aws':
-    ensure   => '1.2.1',
-    provider => 'puppet_gem',
-  } -> package { 'inspec':
-    ensure   => '1.51.6',
-    provider => 'puppet_gem',
+
+  $package_manager_packages.each | String $package_name, $package_details| {
+
+    if $package_details {
+      $package_version = pick(
+        $package_details['version'],
+        'latest'
+      )
+      $package_provider = pick(
+        $package_details['provider'],
+        undef
+      )
+    } else {
+      $package_version = 'latest'
+    }
+
+    if $package_provider {
+      package { $package_name:
+        ensure   => $package_version,
+        provider => $package_provider,
+      }
+    } else {
+      package { $package_name:
+        ensure   => $package_version,
+      }
+    }
   }
 }
