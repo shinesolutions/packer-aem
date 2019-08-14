a9603983b4cdcc19149b7ccf39e1deb212d6465e
Vel <46665089+veldotshine@users.noreply.github.com>
2019-08-15 08:51:22 +1000

    Excluding python-urllib3 from yum upgrading  … (#171)
    * Excluding python-urllib3 from yum upgrading  …
    
    excluding python-urllib3 upgrading from 1.10.2.5 to 1.10.2-7.el7.noarch
    
    * Add comment on why ptyhon-urllib3 is excluded for rhel7.

--- provisioners/bash/rhel7.sh
+++ provisioners/bash/rhel7.sh
@@ -2,7 +2,10 @@
 set -o nounset
 set -o errexit
 
-yum -y upgrade
+# Temporarily exclude python-urllib3 from upgrade due to error with unpacking rpm package python-urllib3-1.10.2-7.el7.noarch
+# `aws: error: unpacking of archive failed on file /usr/lib/python2.7/site-packages/urllib3/packages/ssl_match_hostname: cpio: rename`
+# TODO: re-include python-urllib3
+yum -y upgrade --exclude=python-urllib3-*
 
 rpm -ivh --force https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
 yum -y install puppet-agent epel-release
