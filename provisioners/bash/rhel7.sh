215d0f6b178b07125e7af615e3bfc30468892421
Cliffano Subagio <cliffano@gmail.com>
2019-08-14 15:51:11 -0700

    Add comment on why ptyhon-urllib3 is excluded for rhel7.

--- provisioners/bash/rhel7.sh
+++ provisioners/bash/rhel7.sh
@@ -2,6 +2,9 @@
 set -o nounset
 set -o errexit
 
+# Temporarily exclude python-urllib3 from upgrade due to error with unpacking rpm package python-urllib3-1.10.2-7.el7.noarch
+# `aws: error: unpacking of archive failed on file /usr/lib/python2.7/site-packages/urllib3/packages/ssl_match_hostname: cpio: rename`
+# TODO: re-include python-urllib3
 yum -y upgrade --exclude=python-urllib3-*
 
 rpm -ivh --force https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
