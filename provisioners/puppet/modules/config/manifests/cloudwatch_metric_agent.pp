0826792dd9cf0b87db70cffd6c09b6bac78c9abc
Cliffano Subagio <cliffano@gmail.com>
2019-09-10 01:13:18 +1000

    Fix lint violations.

--- provisioners/puppet/modules/config/manifests/cloudwatch_metric_agent.pp
+++ provisioners/puppet/modules/config/manifests/cloudwatch_metric_agent.pp
@@ -2,13 +2,13 @@ define config::cloudwatch_metric_agent (
   $disk_path, $autoscaling=true
 ) {
   class { '::cloudwatch':
-      enable_mem_util => true,
-      enable_mem_used => true,
+      enable_mem_util        => true,
+      enable_mem_used        => true,
       enable_disk_space_util => true,
       enable_disk_space_used => true,
-      auto_scaling => $autoscaling,
-      cron_min => "*",
-      disk_path => $disk_path,
+      auto_scaling           => $autoscaling,
+      cron_min               => "*",
+      disk_path              => $disk_path,
     }
 
 }
