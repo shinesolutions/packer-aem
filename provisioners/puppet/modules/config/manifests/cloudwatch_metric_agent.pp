16d2611d5cf467fabbe0ebff982b22bfba7b51e5
hoomaan-kh <53849095+hoomaan-kh@users.noreply.github.com>
2019-10-15 13:15:15 +1100

    Disable CloudWatch metrics for memory swap in order to reduce API put metric calls.
    * Fix issue related to cloudwatch EC2 and ELB metric by removing swap_* metrics
    
    * Fix issue related to cloudwatch EC2 and ELB metric by removing swap_* metrics
    
    * Minor change note update.

--- provisioners/puppet/modules/config/manifests/cloudwatch_metric_agent.pp
+++ provisioners/puppet/modules/config/manifests/cloudwatch_metric_agent.pp
@@ -2,13 +2,17 @@ define config::cloudwatch_metric_agent (
   $disk_path, $autoscaling=true
 ) {
   class { '::cloudwatch':
-      enable_mem_util        => true,
-      enable_mem_used        => true,
-      enable_disk_space_util => true,
-      enable_disk_space_used => true,
-      auto_scaling           => $autoscaling,
-      cron_min               => '*',
-      disk_path              => $disk_path,
+      enable_mem_util         => true,
+      enable_mem_used         => true,
+      enable_mem_avail        => true,
+      enable_disk_space_util  => true,
+      enable_disk_space_used  => true,
+      enable_disk_space_avail => true,
+      enable_swap_util        => false,
+      enable_swap_used        => false,
+      auto_scaling            => $autoscaling,
+      cron_min                => '*',
+      disk_path               => $disk_path,
     }
 
 }
