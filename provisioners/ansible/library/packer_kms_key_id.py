0b85afe01c726c2ec216beafed160bb0a7d0e9f7
Cliffano Subagio <cliffano@gmail.com>
2020-02-07 01:33:53 +1100

    Fix invalid KmsKeyId parameter error by moving kms_key_id config to launch block device mapping.

--- provisioners/ansible/library/packer_kms_key_id.py
+++ provisioners/ansible/library/packer_kms_key_id.py
@@ -8,7 +8,7 @@ Input values:
 This will update the Key 'kms_key_id' in the packer template
 {
     Builders: [
-        ami_block_device_mappings: [
+        launch_block_device_mappings: [
             {
             kms_key_id: test
 
@@ -31,15 +31,15 @@ def add_kms_key_id(module, template, kms_key_id):
     """
     Add the kms key id  to the Packer template.
     """
-    # 'ami_block_device_mappings' =
+    # 'launch_block_device_mappings' =
     for builder in template['builders']:
-        if 'ami_block_device_mappings' in builder:
-            if isinstance(builder['ami_block_device_mappings'], list):
-                if isinstance(builder['ami_block_device_mappings'][0], dict):
-                    for ami_block_device_mapping in builder['ami_block_device_mappings']:
+        if 'launch_block_device_mappings' in builder:
+            if isinstance(builder['launch_block_device_mappings'], list):
+                if isinstance(builder['launch_block_device_mappings'][0], dict):
+                    for ami_block_device_mapping in builder['launch_block_device_mappings']:
                         ami_block_device_mapping.update({'kms_key_id': kms_key_id})
         else:
-            module.fail_json(msg="ami_block_device_mappings not found in template at builders.")
+            module.fail_json(msg="launch_block_device_mappings not found in template at builders.")
 def main():
     """
     Run packer_kms_key_id custom module.
