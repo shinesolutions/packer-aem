ded4aa547585a3477cd922d241a3279396b44184
kheirabadih <hoomaan.kheirabadi@auspost.com.au>
2020-07-27 13:25:48 +1000

    Fix puppet version to 5.5.20

--- provisioners/bash/amazon-linux2.sh
+++ provisioners/bash/amazon-linux2.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 set -o nounset
-set -o errexit
+# set -o errexit
 
-# Tomcat 8.x was moved to amazon-linux-extras https://aws.amazon.com/amazon-linux-2/faqs/#Amazon_Linux_Extras
-# need to enable the package in order to access it via yum from Puppet manifest
-# enabling tomcat8.5 will set Tomcat 8.5 as the default tomcat package installation
-amazon-linux-extras enable tomcat8.5
+PUPPET_MAJOR_VERSION=5
+PUPPET_MINOR_VERSION=5
+PUPPET_PATCH_VERSION=20
+PUPPET_AGENT_VERSION="${PUPPET_MAJOR_VERSION}.${PUPPET_MINOR_VERSION}.${PUPPET_PATCH_VERSION}"
+ARCH_TYPE=x86_64
+OS_TYPE=el
+OS_VERSION=7
 
-# yum-utils is needed by Amazon Linux 2 Docker image which doesn't include yum-config-manager
-yum -y install yum-utils
+# Temporarily exclude python-urllib3 from upgrade due to error with unpacking rpm package python-urllib3-1.10.2-7.el7.noarch
+# `aws: error: unpacking of archive failed on file /usr/lib/python2.7/site-packages/urllib3/packages/ssl_match_hostname: cpio: rename`
+# TODO: re-include python-urllib3
+yum remove -y puppet
+yum -y upgrade --exclude=python-urllib3-*
 
-yum -y upgrade
+# install puppet
+# Temporary disabling installation of the latest version of puppet-agent due to an issue with configuring AEM using ruby_aem
+# More information: https://github.com/shinesolutions/packer-aem/issues/218
+#
+# rpm -ivh --force https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
+# yum -y install puppet-agent epel-release
+yum -y install "https://yum.puppetlabs.com/puppet${PUPPET_MAJOR_VERSION}/${OS_TYPE}/${OS_VERSION}/${ARCH_TYPE}/puppet-agent-${PUPPET_AGENT_VERSION}-1.${OS_TYPE}${OS_VERSION}.${ARCH_TYPE}.rpm" || true
+yum -y install epel-release
 
-# install puppet5
-rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
-yum -y install puppet-agent
-
-rpm -ivh --force https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh --force "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}.noarch.rpm"
 yum-config-manager --enable rhui-REGION-rhel-server-optional
 yum-config-manager --enable rhui-REGION-rhel-server-extras
-yum-config-manager --enable rhel-7-server-rhui-optional-rpms
-
-# AEM and Apache httpd provisioning will create users with certain sudo access
-yum install -y sudo
+yum-config-manager --enable "rhel-${OS_VERSION}-server-rhui-optional-rpms"
--- provisioners/bash/centos7.sh
+++ provisioners/bash/centos7.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 set -o nounset
-set -o errexit
+# set -o errexit
 
-yum -y upgrade
+PUPPET_MAJOR_VERSION=5
+PUPPET_MINOR_VERSION=5
+PUPPET_PATCH_VERSION=20
+PUPPET_AGENT_VERSION="${PUPPET_MAJOR_VERSION}.${PUPPET_MINOR_VERSION}.${PUPPET_PATCH_VERSION}"
+ARCH_TYPE=x86_64
+OS_TYPE=el
+OS_VERSION=7
 
-rpm -ivh --force https://yum.puppetlabs.com/puppet5/puppet-release-el-7.noarch.rpm
-yum -y install puppet-agent epel-release
+# Temporarily exclude python-urllib3 from upgrade due to error with unpacking rpm package python-urllib3-1.10.2-7.el7.noarch
+# `aws: error: unpacking of archive failed on file /usr/lib/python2.7/site-packages/urllib3/packages/ssl_match_hostname: cpio: rename`
+# TODO: re-include python-urllib3
+yum remove -y puppet
+yum -y upgrade --exclude=python-urllib3-*
 
-rpm -ivh --force https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# install puppet
+# Temporary disabling installation of the latest version of puppet-agent due to an issue with configuring AEM using ruby_aem
+# More information: https://github.com/shinesolutions/packer-aem/issues/218
+#
+# rpm -ivh --force https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
+# yum -y install puppet-agent epel-release
+yum -y install "https://yum.puppetlabs.com/puppet${PUPPET_MAJOR_VERSION}/${OS_TYPE}/${OS_VERSION}/${ARCH_TYPE}/puppet-agent-${PUPPET_AGENT_VERSION}-1.${OS_TYPE}${OS_VERSION}.${ARCH_TYPE}.rpm" || true
+yum -y install epel-release
+
+rpm -ivh --force "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}.noarch.rpm"
 yum-config-manager --enable rhui-REGION-rhel-server-optional
 yum-config-manager --enable rhui-REGION-rhel-server-extras
-yum-config-manager --enable rhel-7-server-rhui-optional-rpms
-
-# AEM and Apache httpd provisioning will create users with certain sudo access
-yum install -y sudo
-
-# AEM and Apache httpd provisioning are still using service command
-yum install -y initscripts
-(cd /lib/systemd/system/sysinit.target.wants/; for i in *; do [ "$i" == systemd-tmpfiles-setup.service ] || rm -f "$i"; done); \
-rm -f /lib/systemd/system/multi-user.target.wants/*;\
-rm -f /etc/systemd/system/*.wants/*;\
-rm -f /lib/systemd/system/local-fs.target.wants/*; \
-rm -f /lib/systemd/system/sockets.target.wants/*udev*; \
-rm -f /lib/systemd/system/sockets.target.wants/*initctl*; \
-rm -f /lib/systemd/system/basic.target.wants/*;\
-rm -f /lib/systemd/system/anaconda.target.wants/*;
-/usr/sbin/init
+yum-config-manager --enable "rhel-${OS_VERSION}-server-rhui-optional-rpms"
