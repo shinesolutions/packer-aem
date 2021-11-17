f2e048af662aa13a86a018d5c72c2781688427bc
Cliffano Subagio <cliffano@gmail.com>
2021-11-17 18:08:04 +1100

    Update ruby_aem version check.

--- test/inspec/aem_base_spec.rb
+++ test/inspec/aem_base_spec.rb
@@ -14,7 +14,7 @@ describe gem('ruby_aem', '/opt/puppetlabs/puppet/bin/gem') do
   end
 
   it { should be_installed }
-  its('version') { should eq '3.10.1' }
+  its('version') { should eq '3.13.1' }
 end
 
 describe gem('ruby_aem_aws', '/opt/puppetlabs/puppet/bin/gem') do
