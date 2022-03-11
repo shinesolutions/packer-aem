7251021267089463aeba726e696780d85387992c
Stephen Shim <ovlords@users.noreply.github.com>
2022-03-11 15:46:19 +1100

    Upgrade Puppet to 7.9.0 and `aem_curator` to 3.27.0 (#262)
    * Upgrade Puppet to 7.9.0
    
    * Upgrade `aem_curator` to 3.27.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+### Changed
+- Upgrade Puppet to 7.9.0
+- Upgrade `aem_curator` to 3.27.0
 
 ## 5.12.0 - 2022-02-11
 ### Added
--- test/inspec/spec_helper.rb
+++ test/inspec/spec_helper.rb
@@ -6,8 +6,14 @@ require 'hiera'
 def init_conf
   @scope = {
     '::packer_build_name' => Facter.value(:packer_build_name),
+    '::packer_staging_dir' => Facter.value(:packer_staging_dir),
     '::os' => Facter.value(:os)
   }
   # TODO: Fix this when inspec has a path from hiera 3 to 5. More Details: https://tickets.puppetlabs.com/browse/HI-598
-  @hiera = Hiera.new(config: Facter.value(:packer_staging_dir) + '/hiera3.yaml')
+  @hiera = Hiera.new(
+    # Disable packer_staging_dir usage due to nil value being returned since Puppet upgrade to 7.9.0,
+    # replace with string containing the staging directory value for the time being
+    # config: @scope['::packer_staging_dir'] + '/hiera3.yaml'
+    config: '/tmp/shinesolutions/packer-aem/hiera3.yaml'
+  )
 end
